5e729f3d22ca455bf88ae938cc5ee7f9ee9f2065
DESKTOP-UM790HV\Bartolomeo <209255@student.pwr.edu.pl>
2016-10-22 14:29:02 +0200

    Dynamic category list added

---
+++ Assets/Scripts/Category.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+public class Category : MonoBehaviour {
+
+
+    public GameObject katPrefab;
+    
+	// Use this for initialization
+	void Start () {
+        string filePath = Application.streamingAssetsPath;
+        DirectoryInfo dir = new DirectoryInfo(filePath);
+        FileInfo[] files = dir.GetFiles("*.json");
+        foreach(var file in files)
+        {
+            GameObject kat = Instantiate(katPrefab) as GameObject;
+            kat.name = Path.GetFileNameWithoutExtension(file.Name).ToString();
+            kat.transform.SetParent(GameObject.Find("Category/Panel/CategoryContainer").GetComponent<Transform>());
+            kat.GetComponentInChildren<Text>().text = kat.name;
+        }
+	}
+    public void OnClick(string category)
+    {
+        Debug.Log(category);
+    }
+}
