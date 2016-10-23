e662f8be62313dee63ec86fee13f7915307cd0b8
DESKTOP-UM790HV\Bartolomeo <209255@student.pwr.edu.pl>
2016-10-23 19:13:28 +0200

    loading q with a

---
+++ Assets/Scripts/Category.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+public class Category : MonoBehaviour {
+
+
+    public GameObject katPrefab;
+    public MenuManager menu;
+    public Question question;
+	// Use this for initialization
+	void Start () {
+
+        RectTransform rect = gameObject.GetComponent<RectTransform>();
+
+        string filePath = Application.streamingAssetsPath;
+        DirectoryInfo dir = new DirectoryInfo(filePath);
+        FileInfo[] files = dir.GetFiles("*.json");
+        foreach(var file in files)
+        {
+            GameObject kat = Instantiate(katPrefab) as GameObject;
+            kat.name = Path.GetFileNameWithoutExtension(file.Name).ToString();
+            kat.transform.SetParent(GameObject.Find("Category/Panel/Scroll/CategoryContainer").GetComponent<Transform>());
+            kat.GetComponentInChildren<Text>().text = kat.name;
+            kat.GetComponent<Button>().onClick.AddListener(() => OnClick(kat.name));
+            kat.GetComponent<Button>().onClick.AddListener(() => menu.ShowMenu(GameObject.Find("Canvas/Question").GetComponent<Menu>()));
+        }
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x,(rect.sizeDelta.y/6)*files.Length);
+	}
+    public void OnClick(string category)
+    {
+        question.QuestionBegin(category);
+        Debug.Log(category);
+    }
+}
