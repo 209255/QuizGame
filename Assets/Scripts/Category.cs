bb25ebc67646c77431b231df85e1d0a797f1ee53
DESKTOP-UM790HV\Bartolomeo <209255@student.pwr.edu.pl>
2016-10-23 12:27:07 +0200

    fixed game resolution

--- Assets/Scripts/Category.cs
+++ Assets/Scripts/Category.cs
@@ -6,9 +6,12 @@ public class Category : MonoBehaviour {
 
 
     public GameObject katPrefab;
-    
+    public MenuManager menu;
 	// Use this for initialization
 	void Start () {
+
+        RectTransform rect = gameObject.GetComponent<RectTransform>();
+
         string filePath = Application.streamingAssetsPath;
         DirectoryInfo dir = new DirectoryInfo(filePath);
         FileInfo[] files = dir.GetFiles("*.json");
@@ -16,9 +19,12 @@ public class Category : MonoBehaviour {
         {
             GameObject kat = Instantiate(katPrefab) as GameObject;
             kat.name = Path.GetFileNameWithoutExtension(file.Name).ToString();
-            kat.transform.SetParent(GameObject.Find("Category/Panel/CategoryContainer").GetComponent<Transform>());
+            kat.transform.SetParent(GameObject.Find("Category/Panel/Scroll/CategoryContainer").GetComponent<Transform>());
             kat.GetComponentInChildren<Text>().text = kat.name;
+            kat.GetComponent<Button>().onClick.AddListener(() => OnClick(kat.name));
+            kat.GetComponent<Button>().onClick.AddListener(() => menu.ShowMenu(GameObject.Find("Canvas/Soal").GetComponent<Menu>()));
         }
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x,(rect.sizeDelta.y/6)*files.Length);
 	}
     public void OnClick(string category)
     {
