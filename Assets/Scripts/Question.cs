d678753d19e87c12ed13f13329f44eca30f4c771
DESKTOP-UM790HV\Bartolomeo <209255@student.pwr.edu.pl>
2016-11-04 17:37:03 +0100

    dupaczaba

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -10,12 +10,19 @@ public class Question : MonoBehaviour {
     public JsonData questionData;
     public int QuestionNum;
     public GameObject ansPrefab;
-	public void QuestionBegin(string jsonName)
+    public bool nextQuestion;
+    public bool clickQuestion;
+    public int score;
+    public void QuestionBegin(string jsonName)
     {
-        filePath = Path.Combine(Application.streamingAssetsPath, jsonName+".json");
+        score = 0;
+        nextQuestion = true;
+        filePath = Path.Combine(Application.streamingAssetsPath, jsonName + ".json");
         StartCoroutine("Json");
-        questionData = JsonMapper.ToObject (jsonString);
+        questionData = JsonMapper.ToObject(jsonString);
+        GameObject.Find("CategoryTab/Text").GetComponentInChildren<Text>().text = jsonName;
         OnClick();
+        
 
     }
 
@@ -35,37 +42,108 @@ public class Question : MonoBehaviour {
 
     public void OnClick()
     {
-        var ansDestroy = GameObject.FindGameObjectsWithTag("Answer");
-        if(ansDestroy != null)
-        {
-            foreach (var ans in ansDestroy)
-                DestroyImmediate(ans);
-        }
-        GameObject.Find("Question/Panel/QuestionContainer/Question/Text").GetComponentInChildren<Text>().text = questionData["data"][QuestionNum]["question"].ToString();
-
-        for(var i = 0; i <questionData["data"][QuestionNum]["answer"].Count; i++)
-        {
-            var answer = Instantiate(ansPrefab);
-            answer.GetComponentInChildren<Text>().text = questionData["data"][QuestionNum]["answer"][i].ToString();
-            var answerC = GameObject.Find("AnswersContainer").GetComponent<Transform>();
-            answer.transform.SetParent(answerC);
-
-            if(i==0)
-             answer.GetComponent<Button>().onClick.AddListener(() => Answer(1));
-            
-            else
-             answer.GetComponent<Button>().onClick.AddListener(() => Answer(0));
-            answer.transform.SetSiblingIndex(Random.Range(0, 3));
-        }
         
-        QuestionNum++;
+        if (QuestionNum >= questionData["data"].Count)
+        {
+            Debug.Log("Gello");
+            if(score == questionData["data"].Count)
+            {
+                GameObject.Find("Rank").GetComponent<Text>().text = "Excelent";
+            }
+            MenuManager menuResult = GameObject.Find("Canvas").GetComponent<MenuManager>();
+            menuResult.ShowMenu(GameObject.Find("Result").GetComponent<Menu>());
 
+            GameObject.Find("Score").GetComponent<Text>().text = score.ToString()+ "/"+questionData["data"].Count;
+        }
+ 
+        if (nextQuestion)
+        {
+            var ansDestroy = GameObject.FindGameObjectsWithTag("Answer");
+            if (ansDestroy != null)
+            {
+                foreach (var ans in ansDestroy)
+                    DestroyImmediate(ans);
+            }
+            GameObject.Find("Question/Panel/QuestionContainer/Question/Text").GetComponentInChildren<Text>().text = questionData["data"][QuestionNum]["question"].ToString();
+
+            for (var i = 0; i < questionData["data"][QuestionNum]["answer"].Count; i++)
+            {
+                var answer = Instantiate(ansPrefab);
+                answer.GetComponentInChildren<Text>().text = questionData["data"][QuestionNum]["answer"][i].ToString();
+                var answerC = GameObject.Find("AnswersContainer").GetComponent<Transform>();
+                answer.transform.SetParent(answerC);
+
+                string offset = i.ToString();
+                if (i == 0)
+                {
+                    answer.name = "Correct";
+                    answer.GetComponent<Button>().onClick.AddListener(() => Answer("0"));
+                }
+                else
+                {
+                    answer.name = "Wrong" + offset;
+                    answer.GetComponent<Button>().onClick.AddListener(() => Answer(offset));
+                }
+                answer.transform.SetSiblingIndex(Random.Range(0, 3));
+            }
+
+            QuestionNum++;
+            nextQuestion = false;
+            clickQuestion = true;
+            StartCoroutine("Timer");
+        }
     }
-    public void Answer(int x)
+    public void Answer(string questionNum)
     {
-        if (x == 1)
-            Debug.Log("Answer correct");
-        else
-            Debug.Log("Answer wrong");
+        if (clickQuestion)
+        {
+            if (questionNum == "0")
+            {
+                score++;
+                GameObject.Find("Correct").GetComponent<Button>().image.color = Color.green;
+                GameObject.Find("Image ("+QuestionNum+")").GetComponent<Image>().color = Color.green;
+                Debug.Log("Answer correct");
+            }
+            else
+            {
+                GameObject.Find("Wrong" + questionNum).GetComponent<Button>().image.color = Color.red;
+                GameObject.Find("Image ("+QuestionNum+")").GetComponent<Image>().color = Color.red;
+                Debug.Log("Answer wrong");
+            }
+        }
+        nextQuestion = true;
+        clickQuestion = false;
+      
+    }
+    IEnumerator Timer()
+    {
+        Image time = GameObject.Find("Timer").GetComponent<Image>();
+        time.fillAmount = 1;
+        float timeToWait = 3f;
+        float incrementToRemote = 0.05f;
+
+        float delta = time.fillAmount / timeToWait * incrementToRemote;
+
+        while(timeToWait > 0)
+        {
+            yield return new WaitForSeconds(incrementToRemote);
+            if (!nextQuestion)
+            {
+                time.fillAmount -= delta;
+                timeToWait -= incrementToRemote;
+            }
+            else
+                timeToWait = 0;
+        }
+        if(time.fillAmount <=0.1f)
+        {
+            for(int i = 1;i<4; i++)
+                GameObject.Find("Wrong" + i).GetComponent<Button>().image.color = Color.red;
+
+            GameObject.Find("Correct").GetComponent<Button>().image.color = Color.green;
+            GameObject.Find("Image ("+QuestionNum+")").GetComponent<Image>().color = Color.red;
+            clickQuestion = false;
+            nextQuestion = true;
+        }
     }
 }
